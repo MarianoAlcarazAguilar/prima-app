d7be8e9d5ce21aa0fb2ecfeaa264b516a5614933
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-04-04 12:57:56 -0600

    asdf

--- scripts/retrieve_change_logs.sh
+++ scripts/retrieve_change_logs.sh
@@ -3,4 +3,4 @@
 # Este script sirve para sacar los logs del contenedor
 CONTAINER_NAME="prima-values-automation-container"
 
-sudo docker cp $CONTAINER_NAME:/app/logs ./logs
+sudo docker cp $CONTAINER_NAME:/app/logs /home/ubuntu/prima-app/logs
