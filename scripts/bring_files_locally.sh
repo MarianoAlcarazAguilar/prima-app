2d04591e1fc900525d7bbc8e2c52a0a07228261a
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-04-04 12:51:03 -0600

    ahora sí

--- scripts/bring_files_locally.sh
+++ scripts/bring_files_locally.sh
@@ -3,4 +3,4 @@
 # Primero sacamos los datos del contenedor
 ssh -i ~/.ssh/prima-key.pem ubuntu@18.224.199.4 "bash /home/ubuntu/prima-app/scripts/retrieve_change_logs.sh"
 # Luego traemos los documentos a la computadora local
-scp -i ~/.ssh/prima-key.pem -r ubuntu@18.224.199.4:/home/ubuntu/prima-app/logs/logs .
+scp -i ~/.ssh/prima-key.pem -r ubuntu@18.224.199.4:/home/ubuntu/prima-app/logs .
