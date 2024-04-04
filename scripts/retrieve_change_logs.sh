7dac0abe12a7016c5a81185fc6671d66789c7a91
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-04-04 13:00:08 -0600

    a

--- scripts/retrieve_change_logs.sh
+++ scripts/retrieve_change_logs.sh
@@ -3,4 +3,5 @@
 # Este script sirve para sacar los logs del contenedor
 CONTAINER_NAME="prima-values-automation-container"
 
+# Sobreescribimos los logs de la nube al local del ec2
 sudo docker cp $CONTAINER_NAME:/app/logs /home/ubuntu/prima-app/
