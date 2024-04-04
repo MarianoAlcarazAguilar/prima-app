5c9eb993c1cad31794a5ad400463d0691745ba9f
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-04-04 12:43:26 -0600

    moved files

--- scripts/bring_files_locally.sh
+++ scripts/bring_files_locally.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # Primero sacamos los datos del contenedor
-ssh -i ~/.ssh/prima-key.pem ubuntu@18.224.199.4 "bash /home/ubuntu/prima-app/retrieve_change_logs.sh"
+ssh -i ~/.ssh/prima-key.pem ubuntu@18.224.199.4 "bash /home/ubuntu/prima-app/scripts/retrieve_change_logs.sh"
 # Luego traemos los documentos a la computadora local
 scp -i ~/.ssh/prima-key.pem -r ubuntu@18.224.199.4:/home/ubuntu/prima-app/logs/logs .
