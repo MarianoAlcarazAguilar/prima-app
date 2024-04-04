57619d9602eec7ea5f0ec7391c9eaee599e57567
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-04-04 12:39:08 -0600

    traer los cambios a la maquina local

--- retrieve_change_logs.sh
+++ retrieve_change_logs.sh
@@ -4,6 +4,3 @@
 CONTAINER_NAME="prima-values-automation-container"
 
 sudo docker cp $CONTAINER_NAME:/app/logs ./logs
-
-# El comando de abajo es para sacarlo de aws y a mi computadora local
-# scp -i ~/.ssh/prima-key.pem -r ubuntu@18.224.199.4:/home/ubuntu/prima-app/logs/logs .
